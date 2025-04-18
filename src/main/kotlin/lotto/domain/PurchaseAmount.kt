12ca7210dbd40375bf9a7f28e68882a690954b52
권장순 <jsoonworld@gmail.com>
2025-04-18 14:08:44 +0900

    feat(PurchaseAmount) : implement method to calculate ticket count from amount

--- src/main/kotlin/lotto/domain/PurchaseAmount.kt
+++ src/main/kotlin/lotto/domain/PurchaseAmount.kt
@@ -3,12 +3,15 @@ package lotto.domain
 class PurchaseAmount private constructor(private val purchaseAmount: Int) {
 
     init {
-        require(purchaseAmount % 1000 == 0) { ERROR_NOT_THOUSAND_UNIT }
+        require(purchaseAmount % TICKET_PRICE == 0) { ERROR_NOT_THOUSAND_UNIT }
     }
 
     val value: Int get() = purchaseAmount
 
+    fun toTicketCount(): Int = purchaseAmount / TICKET_PRICE
+
     companion object {
+        private const val TICKET_PRICE = 1000
         const val ERROR_NOT_THOUSAND_UNIT = "[ERROR] The purchase amount must be in units of 1,000 KRW."
 
         fun from(amount: Int): PurchaseAmount = PurchaseAmount(amount)
