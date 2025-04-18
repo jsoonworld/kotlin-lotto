61bb4954e50bb1597e4da88e038487edd5d3e136
권장순 <jsoonworld@gmail.com>
2025-04-18 16:11:11 +0900

    feat(OutputView) : add method for WinningBonusNumberInputMessage

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -4,4 +4,5 @@ interface OutputView {
     fun printPurchaseMessage()
     fun printPurchasedTicketCountMessage(count: Int)
     fun printWinningLottoNumbersInputMessage()
+    fun printWinningBonusNumberInputMessage()
 }
