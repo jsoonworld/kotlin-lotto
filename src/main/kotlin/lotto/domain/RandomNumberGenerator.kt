9e0a0bf2b1dbf99b568cba64e5b413af3c74ff4d
권장순 <jsoonworld@gmail.com>
2025-04-18 15:04:16 +0900

    feat(RandomNumberGenerator) : implement lotto and bonus number generation logic

---
+++ src/main/kotlin/lotto/domain/RandomNumberGenerator.kt
@@ -0,0 +1,15 @@
+package lotto.domain
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class RandomNumberGenerator: LottoNumberGenerator {
+    override fun generateLotto(): Lotto {
+        val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+        return Lotto(numbers)
+    }
+
+    override fun generateBonusNumber(exclude: List<Int>): Int {
+        val bonusCandidates = (1..45).toSet() - exclude.toSet()
+        return bonusCandidates.random()
+    }
+}
