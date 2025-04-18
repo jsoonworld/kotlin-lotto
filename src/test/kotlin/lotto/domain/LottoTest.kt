ef1d5f42fb0e1d8e0f79207b90e594790af165d4
권장순 <jsoonworld@gmail.com>
2025-04-18 15:22:03 +0900

    test(Lotto) : move LottoTest from root to domain package

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,6 +1,5 @@
-package lotto
+package lotto.domain
 
-import lotto.domain.Lotto
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -13,7 +12,7 @@ class LottoTest {
 
         @Test
         fun `should create Lotto with 6 unique numbers`() {
-            val lotto = Lotto(listOf(8, 3, 1, 15, 9, 2))
+            val lotto = Lotto.of(listOf(8, 3, 1, 15, 9, 2))
             assertThat(lotto.numbers()).containsExactly(1, 2, 3, 8, 9, 15)
         }
     }
@@ -24,7 +23,7 @@ class LottoTest {
         @Test
         fun `should throw when more than 6 numbers are given`() {
             val exception = assertThrows<IllegalArgumentException> {
-                Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+                Lotto.of(listOf(1, 2, 3, 4, 5, 6, 7))
             }
 
             assertThat(exception.message).isEqualTo(Lotto.ERROR_INVALID_SIZE)
@@ -33,7 +32,7 @@ class LottoTest {
         @Test
         fun `should throw when duplicate numbers are given`() {
             val exception = assertThrows<IllegalArgumentException> {
-                Lotto(listOf(1, 2, 3, 4, 5, 5))
+                Lotto.of(listOf(1, 2, 3, 4, 5, 5))
             }
 
             assertThat(exception.message).isEqualTo(Lotto.ERROR_DUPLICATE_NUMBERS)
