8ea00b258db78112d29c1c9d1d38969e07cf1c97
권장순 <jsoonworld@gmail.com>
2025-04-18 16:18:05 +0900

    feat(ConsoleInputView) : add method for WinningBonusNumber

--- src/main/kotlin/lotto/view/ConsoleInputView.kt
+++ src/main/kotlin/lotto/view/ConsoleInputView.kt
@@ -5,4 +5,5 @@ import camp.nextstep.edu.missionutils.Console
 class ConsoleInputView: InputView {
     override fun readPurchaseAmount(): String = Console.readLine()
     override fun readWinningLottoNumbers(): String = Console.readLine()
+    override fun readWinningBonusNumber(): String = Console.readLine()
 }
