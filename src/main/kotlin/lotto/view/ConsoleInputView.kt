3cbcf0b6ece539cb61c9ae075cfe2f328f6d91c1
권장순 <jsoonworld@gmail.com>
2025-04-18 16:05:02 +0900

    refactor(ConsoleInputView) : rename readWinningNumbers implementation to match interface

--- src/main/kotlin/lotto/view/ConsoleInputView.kt
+++ src/main/kotlin/lotto/view/ConsoleInputView.kt
@@ -4,5 +4,5 @@ import camp.nextstep.edu.missionutils.Console
 
 class ConsoleInputView: InputView {
     override fun readPurchaseAmount(): String = Console.readLine()
-    override fun readWinningNumbers(): String = Console.readLine()
+    override fun readWinningLottoNumbers(): String = Console.readLine()
 }
