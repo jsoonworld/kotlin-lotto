ab5424a4295eb7e8b447d6fce7d24ff56b13d574
권장순 <jsoonworld@gmail.com>
2025-04-18 13:59:44 +0900

    feat(ConsoleOutputView) : add ticket count output and purchase message

--- src/main/kotlin/lotto/view/ConsoleOutputView.kt
+++ src/main/kotlin/lotto/view/ConsoleOutputView.kt
@@ -1,11 +1,16 @@
 package lotto.view
 
 class ConsoleOutputView : OutputView{
-    override fun printPurchasePrompt() {
-        println(PURCHASE_AMOUNT_PROMPT)
+    override fun printPurchaseMessage() {
+        println(PURCHASE_AMOUNT_MESSAGE)
+    }
+
+    override fun printPurchasedTicketCountMessage(count: Int) {
+        println(PURCHASED_TICKET_COUNT_MESSAGE.format(count))
     }
 
     companion object {
-        const val PURCHASE_AMOUNT_PROMPT = "Please enter the purchase amount."
+        const val PURCHASE_AMOUNT_MESSAGE = "Please enter the purchase amount."
+        const val PURCHASED_TICKET_COUNT_MESSAGE = "You have purchased %d tickets."
     }
 }
