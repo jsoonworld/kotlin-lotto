07325fd895ee84999d266873001e3822f884438d
권장순 <jsoonworld@gmail.com>
2025-04-18 16:11:47 +0900

    feat(ConsoleOutputView) : add method for WinningBonusNumberInputMessage

--- src/main/kotlin/lotto/view/ConsoleOutputView.kt
+++ src/main/kotlin/lotto/view/ConsoleOutputView.kt
@@ -10,12 +10,17 @@ class ConsoleOutputView : OutputView{
     }
 
     override fun printWinningLottoNumbersInputMessage() {
-        println(WINNING_NUMBERS_INPUT_MESSAGE)
+        println(WINNING_LOTTO_NUMBERS_INPUT_MESSAGE)
+    }
+
+    override fun printWinningBonusNumberInputMessage() {
+        println(WINNING_BONUS_NUMBER_INPUT_MESSAGE)
     }
 
     companion object {
         const val PURCHASE_AMOUNT_MESSAGE = "Please enter the purchase amount."
         const val PURCHASED_TICKET_COUNT_MESSAGE = "You have purchased %d tickets."
-        const val WINNING_NUMBERS_INPUT_MESSAGE = "Please enter the winning numbers."
+        const val WINNING_LOTTO_NUMBERS_INPUT_MESSAGE = "Please enter the winning numbers."
+        const val WINNING_BONUS_NUMBER_INPUT_MESSAGE = "Please enter the bonus number."
     }
 }
