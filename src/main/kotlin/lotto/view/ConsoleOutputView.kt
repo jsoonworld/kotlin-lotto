03d189981ecc609ace991da271b71f84f8f58b97
권장순 <jsoonworld@gmail.com>
2025-04-18 16:25:58 +0900

    feat(ConsoleOutputView) : add method for WinningStatisticsMessage

--- src/main/kotlin/lotto/view/ConsoleOutputView.kt
+++ src/main/kotlin/lotto/view/ConsoleOutputView.kt
@@ -17,10 +17,17 @@ class ConsoleOutputView : OutputView{
         println(WINNING_BONUS_NUMBER_INPUT_MESSAGE)
     }
 
+    override fun printWinningStatisticsMessage() {
+        println(WINNING_STATISTICS_MESSAGE)
+        println(STATISTICS_SEPARATOR)
+    }
+
     companion object {
         const val PURCHASE_AMOUNT_MESSAGE = "Please enter the purchase amount."
         const val PURCHASED_TICKET_COUNT_MESSAGE = "You have purchased %d tickets."
         const val WINNING_LOTTO_NUMBERS_INPUT_MESSAGE = "Please enter the winning numbers."
         const val WINNING_BONUS_NUMBER_INPUT_MESSAGE = "Please enter the bonus number."
+        const val WINNING_STATISTICS_MESSAGE = "Winning Statistics"
+        const val STATISTICS_SEPARATOR = "---"
     }
 }
